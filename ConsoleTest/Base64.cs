4d7ef198c9a014cc34a292a911c4394769a16654
jiecong <jiecong@outlook.com>
2019-08-14 18:08:18 +0800

    .

---
+++ ConsoleTest/Base64.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    /// <summary>
+    /// 实现Base64加密解密
+    /// 作者：周公
+    /// 时间：2007
+    /// </summary>
+    public sealed class Base64
+    {
+        /// <summary>
+        /// Base64加密
+        /// </summary>
+        /// <param name="codeName">加密采用的编码方式</param>
+        /// <param name="source">待加密的明文</param>
+        /// <returns></returns>
+        public static string EncodeBase64(Encoding encode, string source)
+        {
+            string s = "";
+            byte[] bytes = encode.GetBytes(source);
+            try
+            {
+                s = Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                s = source;
+            }
+            return s;
+        }
+
+        public static string EncodeBase64(Encoding encode, byte[] bytes)
+        {
+            string s = "";
+            try
+            {
+                s = Convert.ToBase64String(bytes);
+            }
+            catch
+            {
+                s = "";
+            }
+            return s;
+        }
+
+        /// <summary>
+        /// Base64加密，采用utf8编码方式加密
+        /// </summary>
+        /// <param name="source">待加密的明文</param>
+        /// <returns>加密后的字符串</returns>
+        public static string EncodeBase64(string source)
+        {
+            return EncodeBase64(Encoding.UTF8, source);
+        }
+
+        /// <summary>
+        /// Base64解密
+        /// </summary>
+        /// <param name="codeName">解密采用的编码方式，注意和加密时采用的方式一致</param>
+        /// <param name="result">待解密的密文</param>
+        /// <returns>解密后的字符串</returns>
+        public static string DecodeBase64(Encoding encode, string result)
+        {
+            string decode = "";
+            byte[] bytes = Convert.FromBase64String(result);
+            try
+            {
+                decode = encode.GetString(bytes);
+            }
+            catch
+            {
+                decode = result;
+            }
+            return decode;
+        }
+
+        //public static byte[] DecodeBase64(Encoding encode, string result)
+        //{
+
+        //    byte[] bytes = null;
+        //    try
+        //    {
+        //        bytes=Convert.FromBase64String(result);
+        //    }
+        //    catch
+        //    {
+        //        bytes = null;
+        //    }
+        //    return bytes;
+        //}
+
+    }
+}
