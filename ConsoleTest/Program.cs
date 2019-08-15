6244687addd13c9674251866bce4dab2fd4a2fec
jiecong <jiecong@outlook.com>
2019-08-15 18:14:21 +0800

    .

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -17,6 +17,16 @@ using System.Threading.Tasks;
 
 namespace ConsoleTest
 {
+    public class userInfo
+    {
+        public string channelUserCode { get; set; }
+        public string userName { get; set; }
+        public string phone { get; set; }
+        public string idName { get; set; }
+        public string email { get; set; }
+        public string channelCompanyCode { get; set; }
+    }
+
     class Program
     {
         private static AsymmetricKeyParameter GetPublicKeyParameter(string s)
@@ -41,11 +51,24 @@ namespace ConsoleTest
 
         static void Main(string[] args)
         {
-            String input = "{\"channelUserCode\":\"15989287032\",\"userName\":\"15989287032\",\"phone\":\"15989287032\",\"idName\":\"\",\"email\":\"\",\"channelCompanyCode\":\"\"}";
+            String input = "{channelUserCode:\"15989287032\",userName:\"15989287032\",phone:\"15989287032\",idName:\"\",email:\"\",channelCompanyCode:\"\"}";
 
+            userInfo u = new userInfo();
+
+            u.channelUserCode = "15989287032";
+            u.userName = "15989287032";
+            u.phone = "15989287032";
+            u.idName = "";
+            u.email = "";
+            u.channelCompanyCode = "";
+
+
+            input = Newtonsoft.Json.JsonConvert.SerializeObject(u);
+
+            input = input.Replace("\\", "");
             //// 公钥
-            String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCuTexchfJUXLJR5cSevbQutT9dYj95tW8NuH/Bc5JZwGn/WOr1Vw6jg7ncVTilPRjIS5XqmcTFm6H0qa/uZ3Vn1Mh+XZkfX6hcxmeTQm9vkv3mb4hpWRFhfri3gcbmkQr+DnklqoI7TGWcuYQt47BeppGeeKT7WYblz+lZvaqepQIDAQAB";
-            //String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDTT1ryGLfq5lucyHdzPLbjtcVsgurf5x4Y09U/cTiV85duIk0zQeRTXNyGcMAS92+xV/eGp7IjncwL8QE8JqlclLvuOU3zTdlAQ58lu/JcTcsF6eA6JXb8OJAhmDoug1J77M2GLoqAl0Cf34kavj/r9bAQpWqbk8JlJU3YqIePuwIDAQAB";
+            //String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCuTexchfJUXLJR5cSevbQutT9dYj95tW8NuH/Bc5JZwGn/WOr1Vw6jg7ncVTilPRjIS5XqmcTFm6H0qa/uZ3Vn1Mh+XZkfX6hcxmeTQm9vkv3mb4hpWRFhfri3gcbmkQr+DnklqoI7TGWcuYQt47BeppGeeKT7WYblz+lZvaqepQIDAQAB";
+            String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDTT1ryGLfq5lucyHdzPLbjtcVsgurf5x4Y09U/cTiV85duIk0zQeRTXNyGcMAS92+xV/eGp7IjncwL8QE8JqlclLvuOU3zTdlAQ58lu/JcTcsF6eA6JXb8OJAhmDoug1J77M2GLoqAl0Cf34kavj/r9bAQpWqbk8JlJU3YqIePuwIDAQAB";
 
             //// 私钥
             String privateKey = "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAK5N7FyF8lRcslHlxJ69tC61P11iP3m1bw24f8FzklnAaf9Y6vVXDqODudxVOKU9GMhLleqZxMWbofSpr+5ndWfUyH5dmR9fqFzGZ5NCb2+S/eZviGlZEWF+uLeBxuaRCv4OeSWqgjtMZZy5hC3jsF6mkZ54pPtZhuXP6Vm9qp6lAgMBAAECgYEApMJLdXm3gj7M39UMcfBnbO8uIhtIXMc/XfzT5gxUfjn+97sY/Sd5Ut6kxLxZevexgULRRpxq/08JW7c58WQh+J+ORdoqPwEyvINcvvAaFtJ0ydIhTi3I4yfmgmgqCdC7stuS6kDB+LO+8nRpNwBEhBIZzzgZcD6qc4sXdS1gD9ECQQDHiBpQl8rJkyTwEV9J0h4bE8oBSuHrIyVRv4VW/l11rBwYh9NWeB+CZy9x6IzsbNOApopUEdLBr00PWGrejkpvAkEA36IgeXEJB6t3+0JwrqvvaQXH94JPHZDNvExQ1fMXlYEmqZEXzANkLu1syVYwTs8FsM/hosdTZ8dBhF43zGTCKwJASNKswANWeNFiZtgATiII6NsFHAmngLk4Eqjy0nhNxffF3VIdWO7ImUBtuYYlgNiLLOYbkGlc4WHInzQm9Qk7swJBAJGVUd69H0vG7Iy9a+3KMEkGYm9WfXqZ0dVLOTSO1EOXmDu7IOrHKmkCV1earEghrWq1agY2DK36oUQysdB1p5ECQQCT+PDLPTmvJ8OtyQknPOyFDyG/yJB7MsT6Ylr9Rhna0+PqSfgY2hvxK6aHam3sMscPCRYjHI/ptYpHgznhY8uz";
