903a0180c1361c601476b10eb450a4a8c2d5e0dc
qqw <qqw@uplink.net.cn>
2019-08-31 22:31:14 +0800

    as

--- MiniProgramStoreApp2/pages/test/test.js
+++ MiniProgramStoreApp2/pages/test/test.js
@@ -1,160 +1,109 @@
-
-var app = getApp()
-
 Page({
 
+  /**
+   * 页面的初始数据
+   */
   data: {
-
-    items: [],
-
-    startX: 0, //开始坐标
-
-    startY: 0
-
+    boolHidden: false,
+    animationData: {}
   },
 
-  onLoad: function (e) {
-
-    var that = this;
-
-    //common是自己写的公共JS方法，可忽略
-
-    //common.sys_main(app, that, e);
-
-    for (var i = 0; i < 10; i++) {
-
-      this.data.items.push({
-
-        content: i + " 向左滑动删除哦,向左滑动删除哦,向左滑动删除哦,向左滑动删除哦,向左滑动删除哦",
-
-        isTouchMove: false //默认隐藏删除
-
-      })
-
-    }
-
-    this.setData({
-
-      items: this.data.items
-
-    });
-
-  }
-
-  ,
-
-  //手指触摸动作开始 记录起点X坐标
-
-  touchstart: function (e) {
-
-    //开始触摸时 重置所有删除
-
-    this.data.items.forEach(function (v, i) {
-
-      if (v.isTouchMove)//只操作为true的
-
-        v.isTouchMove = false;
-
-    })
-
-    this.setData({
-
-      startX: e.changedTouches[0].clientX,
-
-      startY: e.changedTouches[0].clientY,
-
-      items: this.data.items
-
-    })
-
-  },
-
-  //滑动事件处理
-
-  touchmove: function (e) {
-
-    var that = this,
-
-      index = e.currentTarget.dataset.index,//当前索引
-
-      startX = that.data.startX,//开始X坐标
-
-      startY = that.data.startY,//开始Y坐标
-
-      touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
-
-      touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
-
-      //获取滑动角度
-
-      angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
-
-    that.data.items.forEach(function (v, i) {
-
-      v.isTouchMove = false
-
-      //滑动超过30度角 return
-
-      if (Math.abs(angle) > 30) return;
-
-      if (i == index) {
-
-        if (touchMoveX > startX) //右滑
-
-          v.isTouchMove = false
-
-        else //左滑
-
-          v.isTouchMove = true
-
-      }
-
-    })
-
-    //更新数据
-
-    that.setData({
-
-      items: that.data.items
-
-    })
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
 
   },
 
   /**
-  
-  * 计算滑动角度
-  
-  * @param {Object} start 起点坐标
-  
-  * @param {Object} end 终点坐标
-  
-  */
-
-  angle: function (start, end) {
-
-    var _X = end.X - start.X,
-
-      _Y = end.Y - start.Y
-
-    //返回角度 /Math.atan()返回数字的反正切值
-
-    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
 
   },
 
-  //删除事件
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
 
-  del: function (e) {
+  },
 
-    this.data.items.splice(e.currentTarget.dataset.index, 1)
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
 
-    this.setData({
+  },
 
-      items: this.data.items
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
 
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  // 打开购物车
+  openCar: function (e) {
+    // 用that取代this，防止不必要的情况发生
+    var that = this;
+    // 创建一个动画实例
+    var animation = wx.createAnimation({
+      // 动画持续时间
+      duration: 300,
+      // 定义动画效果，当前是匀速
+      timingFunction: 'linear'
     })
-
+    // 将该变量赋值给当前动画
+    that.animation = animation
+    // 先在y轴偏移，然后用step()完成一个动画
+    animation.translateY(300).step()
+    // 用setData改变当前动画
+    that.setData({
+      // 通过export()方法导出数据
+      animationData: animation.export(),
+      // 改变view里面的Wx：if
+      boolHidden: true
+    })
+    // 设置setTimeout来改变y轴偏移量，实现有感觉的滑动
+    setTimeout(function () {
+      animation.translateY(0).step()
+      that.setData({
+        animationData: animation.export()
+      })
+    }, 200)
+  },
+  // 关闭购物车
+  closeHide: function (e) {
+    var that = this;
+    var animation = wx.createAnimation({
+      duration: 300,
+      timingFunction: 'linear'
+    })
+    that.animation = animation
+    animation.translateY(300).step()
+    that.setData({
+      animationData: animation.export()
+    });
   }
-
 })
