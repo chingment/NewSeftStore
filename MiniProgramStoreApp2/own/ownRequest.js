25e0019e867c0428d9cce3e807c7248394e4fe92
qqw <qqw@uplink.net.cn>
2019-09-17 20:01:56 +0800

    as

--- MiniProgramStoreApp2/own/ownRequest.js
+++ MiniProgramStoreApp2/own/ownRequest.js
@@ -79,98 +79,6 @@ function isSelectedStore(isGoSelect) {
   }
 }
 
-function login(callback) {
-  wx.showLoading()
-  wx.login({
-    success(res) {
-      if (res.code) {
-        // 登录成功，获取用户信息
-        getUserInfo(res.code, callback)
-      } else {
-        // 否则弹窗显示，showToast需要封装
-        showToast()
-      }
-    },
-    fail() {
-      showToast()
-    }
-  })
-}
-
-// 获取用户信息
-function getUserInfo(code, callback) {
-  wx.getUserInfo({
-    // 获取成功，全局存储用户信息，开发者服务器登录
-    success(res) {
-      //console.log("code:" + code)
-      //console.log("iv:" + res.iv)
-      //console.log("encryptedData:" + res.encryptedData)
-      // console.log("userInfo:" + JSON.stringify(res))
-      // 全局存储用户信息
-      //store.commit('storeUpdateWxUser', res.userInfo)
-      // postLogin(code, res.iv, res.encryptedData, callback)
-
-
-      let params = {
-        code: code,
-        iv: res.iv,
-        encryptedData: res.encryptedData
-      }
-      callback && callback(params)
-      // lumos.postJson({
-      //   url: config.apiUrl.userLoginByMinProgram, dataParams: params,
-      //   success: function (res) {
-      //     if (res.result == 1) {
-      //       storeage.setAccessToken(res.data.accessToken);
-      //       console.log("getAccessonToken" + storeage.getAccessToken())
-      //       callback && callback()
-      //     }
-      //   },
-      //   fail: function () {
-      //     showToast()
-      //   }
-      // })
-    },
-    // 获取失败，弹窗提示一键登录
-    fail() {
-      wx.hideLoading()
-      // 获取用户信息失败，清楚全局存储的登录状态，弹窗提示一键登录
-      // 使用token管理登录态的，清楚存储全局的token
-      // 使用cookie管理登录态的，可以清楚全局登录状态管理的变量
-      //store.commit('storeUpdateToken', '')
-      // 获取不到用户信息，说明用户没有授权或者取消授权。弹窗提示一键登录，后续会讲
-      showLoginModal()
-    }
-  })
-}
-
-
-function showLoginModal() {
-  wx.showModal({
-    title: '提示',
-    content: '你还未登录，登录后可获得完整体验 ',
-    showCancel: false,
-    confirmText: '一键登录',
-    success(res) {
-      // 点击一键登录，去授权页面
-      if (res.confirm) {
-        wx.navigateTo({
-          url: loginPage,
-        })
-      }
-    }
-  })
-}
-
-function showToast(content = '登录失败，请稍后再试') {
-  wx.showToast({
-    title: content,
-    icon: 'none'
-  })
-}
-
-
-
 module.exports = {
   goLogin: goLogin,
   getCurrentStoreId: getCurrentStoreId,
@@ -178,6 +86,5 @@ module.exports = {
   getCurrentStore: getCurrentStore,
   isSelectedStore: isSelectedStore,
   isLogin: isLogin,
-  login: login,
   getReturnUrl: getReturnUrl
 }
--- MiniProgramStoreApp2/pages/login/login.js
+++ MiniProgramStoreApp2/pages/login/login.js
@@ -4,103 +4,55 @@ const storeage = require('../../utils/storeageutil.js')
 const ownRequest = require('../../own/ownRequest.js')
 const apiOwn = require('../../api/own.js')
 
-// pages/login/login.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function(options) {
-
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function() {
-
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function() {
-
-
   },
-
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  },
-
-
   bindgetuserinfo: function(e) {
-    var pages = getCurrentPages() //获取加载的页面
-    var currentPage = pages[pages.length - 2] //获取当前页面的对象
-    var url = currentPage.route //当前页面ur
+    if (e.detail.userInfo) {
+      wx.login({
+        success(res) {
+          if (res.code) {
 
-
-    ownRequest.login((params) => {
-      apiOwn.loginByMinProgram({
-        merchId: config.merchId,
-        appId: config.appId,
-        code: params.code,
-        iv: params.iv,
-        encryptedData: params.encryptedData
-      }, {
-        success: function(res) {
-          if (res.result == 1) {
-            storeage.setAccessToken(res.data.token);
-            wx.reLaunch({ //关闭所有页面，打开到应用内的某个页面
-              url: ownRequest.getReturnUrl()
-            })
+            apiOwn.loginByMinProgram({
+              merchId: config.merchId,
+              appId: config.appId,
+              code: res.code,
+              iv: e.detail.iv,
+              encryptedData: e.detail.encryptedData
+            }, {
+                success: function (res) {
+                  if (res.result == 1) {
+                    storeage.setAccessToken(res.data.token);
+                    wx.reLaunch({ //关闭所有页面，打开到应用内的某个页面
+                      url: ownRequest.getReturnUrl()
+                    })
+                  } else {
+                    toast.show({
+                      title: res.message
+                    })
+                  }
+                },
+                fail: function () { }
+              })
+        
           } else {
-            toast.show({
-              title: res.message
-            })
+            
           }
         },
-        fail: function() {}
+        fail() {
+         
+        }
       })
-    })
+    } else {
+      toast.show({
+        title: '您点击了拒绝,为了更好的体验，请点击允许授权'
+      })
+    }
   }
 })
