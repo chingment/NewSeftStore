cdccc7bf26ecc9a6c3e9c1700ca3d36aed30f79c
qiuqingwen <chingment@126.com>
2019-12-04 11:44:55 +0800

    .

---
+++ WebUploadServer/Controllers/UploadController.cs
@@ -0,0 +1,45 @@
+﻿using log4net;
+using Lumos;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace WebUploadImageServer.Controllers
+{
+    public class UploadController : ApiController
+    {
+        private static ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        public HttpResponseMessage Post()
+        {
+            log.Info("调用Upload.Post");
+
+            HttpContextBase context = (HttpContextBase)Request.Properties["MS_HttpContext"];//获取传统context
+            HttpRequestBase request = context.Request;//定义传统request对象 
+            CustomJsonResult r = new CustomJsonResult();
+
+            log.Info("文件Count:" + request.Files.Count);
+            for (var i = 0; i < request.Files.Count; i++)
+            {
+                log.Info("文件名称:" + request.Files[i].FileName);
+            }
+
+            log.Info("表单Count:" + request.Form.Count);
+            for (var i = 0; i < request.Form.Count; i++)
+            {
+                log.Info("文件名称:" + request.Form[i]);
+            }
+
+
+            string json = r.ToString();
+            HttpResponseMessage result = new HttpResponseMessage { Content = new StringContent(json, Encoding.GetEncoding("UTF-8"), "application/json") };
+            return result;
+        }
+    }
+}
