e2977dd6a30aecd232dc928145164d02c4132561
qiuqingwen <chingment@126.com>
2019-12-05 11:12:34 +0800

    .

--- WebUploadServer/Controllers/UploadController.cs
+++ WebUploadServer/Controllers/UploadController.cs
@@ -24,18 +24,74 @@ namespace WebUploadServer.Controllers
             HttpRequestBase request = context.Request;//定义传统request对象 
             CustomJsonResult r = new CustomJsonResult();
 
-            log.Info("文件Count:" + request.Files.Count);
-            for (var i = 0; i < request.Files.Count; i++)
-            {
-                log.Info("文件名称:" + request.Files[i].FileName);
-            }
 
-            log.Info("表单Count:" + request.Form.Count);
-            for (var i = 0; i < request.Form.Count; i++)
+            try
             {
-                log.Info("文件名称:" + request.Form[i]);
-            }
+                string domain = System.Configuration.ConfigurationManager.AppSettings["custom:FilesServerUrl"];
 
+                log.Info("文件Count:" + request.Files.Count);
+                for (var i = 0; i < request.Files.Count; i++)
+                {
+                    log.Info("文件名称:" + request.Files[i].FileName);
+                }
+
+                log.Info("表单Count:" + request.Form.Count);
+                for (var i = 0; i < request.Form.Count; i++)
+                {
+                    log.Info("文件名称:" + request.Form[i]);
+                }
+
+                string savePath = "/Upload/";
+                string uploadFolder = "common";
+                if (!string.IsNullOrEmpty(request.Form["folder"]))
+                {
+                    uploadFolder = request.Form["folder"];
+                    log.Info("uploadFolder:" + uploadFolder);
+                }
+
+                savePath += uploadFolder;
+
+                DirectoryInfo dir = new DirectoryInfo(savePath);
+                if (!dir.Exists)
+                {
+                    dir.Create();
+                }
+
+                byte[] fileData = null;
+                using (var binaryReader = new BinaryReader(request.Files[0].InputStream))
+                {
+                    fileData = binaryReader.ReadBytes(request.Files[0].ContentLength);
+                }
+                if (fileData != null && fileData.Length > 0)
+                {
+                    string fileName = request.Files[0].FileName;
+                    log.Info("fileName:" + fileName);
+
+                    string roorPath = System.Configuration.ConfigurationManager.AppSettings["custom:FileServerUploadPath"];
+
+                    string serverSavePath = roorPath + "/" + savePath + "/" + fileName;
+                    string domainSavePath = domain + "/" + savePath + "/" + fileName;
+
+                    log.Info("serverSavePath:" + serverSavePath);
+                    log.Info("domainSavePath:" + domainSavePath);
+                    FileStream fs = new FileStream(serverSavePath, FileMode.Create, FileAccess.Write);
+                    fs.Write(fileData, 0, fileData.Length);
+                    fs.Flush();
+                    fs.Close();
+
+                    r.Result = ResultType.Success;
+                    r.Code = ResultCode.Success;
+                    r.Data = new { name = fileName, url = domainSavePath };
+                    r.Message = "上传成功";
+                }
+            }
+            catch (Exception ex)
+            {
+                r.Code = ResultCode.Exception;
+                r.Result = ResultType.Exception;
+                r.Message = "上传失败";
+                LogUtil.Error("WebApi上传图片异常", ex);
+            }
 
             string json = r.ToString();
             HttpResponseMessage result = new HttpResponseMessage { Content = new StringContent(json, Encoding.GetEncoding("UTF-8"), "application/json") };
