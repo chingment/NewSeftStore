da158ecd704b2eea18934707a7b721b08061f034
jiecong <jiecong@outlook.com>
2020-10-18 08:41:17 +0800

    .

--- LocalS.Service/Api/StoreTerm/RModels/RopStockSettingSaveCabinetSlot.cs
+++ LocalS.Service/Api/StoreTerm/RModels/RopStockSettingSaveCabinetSlot.cs
@@ -15,6 +15,8 @@ namespace LocalS.Service.Api.StoreTerm
         public string ProductSkuId { get; set; }
         public int SumQuantity { get; set; }
         public int MaxQuantity { get; set; }
+        public int WarnQuantity { get; set; }
+        public int HoldQuantity { get; set; }
         public int Version { get; set; }
     }
 }
