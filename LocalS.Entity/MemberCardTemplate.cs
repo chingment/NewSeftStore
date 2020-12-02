154809b08c5a1bc74f998f5c5e22d2e1ff798ed2
jiecong <jiecong@outlook.com>
2020-12-02 18:09:49 +0800

    .

---
+++ LocalS.Entity/MemberCardTemplate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LocalS.Entity
+{
+    [Table("MemberCardTemplate")]
+    public class MemberCardTemplate
+    {
+        [Key]
+        public string Id { get; set; }
+        public string MerchId { get; set; }
+        [MaxLength(128)]
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Fee { get; set; }
+        public string Creator { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string Mender { get; set; }
+        public DateTime? MendTime { get; set; }
+    }
+}
