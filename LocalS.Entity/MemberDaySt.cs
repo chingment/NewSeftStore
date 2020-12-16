90a0346a461cc219e373de487397d626f8ed6228
qiuqingwen <chingment@126.com>
2020-12-16 17:08:26 +0800

    .

--- LocalS.Entity/MemberDaySt.cs
+++ LocalS.Entity/MemberDaySt.cs
@@ -4,16 +4,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LocalS.Entity
 {
-    [Table("MemberRightSt")]
-    public class MemberBagSt
+
+    [Table("MemberDaySt")]
+    public class MemberDaySt
     {
         [Key]
         public string Id { get; set; }
         public string MerchId { get; set; }
-        public string LevelId { get; set; }
-        public string FeeId { get; set; }
-        public string RightType { get; set; }
-        public string RightValue { get; set; }
+        public string Day { get; set; }
+        public int MemberLevel { get; set; }
+        public decimal Discount { get; set; }
         public string Creator { get; set; }
         public DateTime CreateTime { get; set; }
         public string Mender { get; set; }
