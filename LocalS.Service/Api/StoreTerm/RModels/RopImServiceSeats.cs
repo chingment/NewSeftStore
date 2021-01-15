2066fff9d4e018914dc9f9b03f6f4621a614980e
jiecong <jiecong@outlook.com>
2021-01-15 10:05:50 +0800

    .

--- LocalS.Service/Api/StoreTerm/RModels/RopImServiceSeats.cs
+++ LocalS.Service/Api/StoreTerm/RModels/RopImServiceSeats.cs
@@ -9,7 +9,6 @@ namespace LocalS.Service.Api.StoreTerm
     public class RopImServiceSeats
     {
         public string MachineId { get; set; }
-
         public List<ProductSku> ProductSkus { get; set; }
         public class ProductSku
         {
