8e012ddd8b9e07330254243452483cf7ea5403f4
jiecong <jiecong@outlook.com>
2021-01-20 17:15:36 +0800

    .

--- LocalS.BLL/Task/Task4Tim2GlobalProvider.cs
+++ LocalS.BLL/Task/Task4Tim2GlobalProvider.cs
@@ -25,8 +25,9 @@ namespace LocalS.BLL.Task
 
     public class Task4Tim2GlobalProvider : BaseService, IJob
     {
-        private static readonly string key = "task4Tim2Global";
+        private static readonly string TAG = "Task4Tim2GlobalProvider";
 
+        private static readonly string key = "task4Tim2Global";
 
         public void Enter(Task4TimType type, string id, DateTime expireTime, object data)
         {
@@ -75,32 +76,34 @@ namespace LocalS.BLL.Task
             try
             {
                 var lists = GetList();
-                LogUtil.Info(string.Format("共有{0}条记录需要检查状态", lists.Count));
+                LogUtil.Info(TAG, string.Format("共有{0}条记录需要检查状态", lists.Count));
                 if (lists.Count > 0)
                 {
                     foreach (var m in lists)
                     {
+                        LogUtil.SetTrackId();
+
                         switch (m.Type)
                         {
                             case Task4TimType.Order2CheckReservePay:
                                 #region 检查支付状态
-                                LogUtil.Info(string.Format("开始执行订单查询,时间：{0}", DateTime.Now));
+                                LogUtil.Info(TAG, string.Format("开始执行订单查询,时间：{0}", DateTime.Now));
                                 var order = m.Data.ToJsonObject<Order2CheckPayModel>();
-                                LogUtil.Info(string.Format("查询订单号：{0}", order.Id));
+                                LogUtil.Info(TAG, string.Format("查询订单号：{0}", order.Id));
                                 //判断支付过期时间
                                 if (m.ExpireTime.AddMinutes(1) < DateTime.Now)
                                 {
-                                    LogUtil.Info(string.Format("订单号：{0},订单支付有效时间过期", order.Id));
+                                    LogUtil.Info(TAG, string.Format("订单号：{0},订单支付有效时间过期", order.Id));
                                     BizFactory.Order.Cancle(IdWorker.Build(IdType.EmptyGuid), order.Id, E_OrderCancleType.PayTimeout, "订单支付有效时间过期");
                                 }
-                                LogUtil.Info(string.Format("结束执行订单查询,时间:{0}", DateTime.Now));
+                                LogUtil.Info(TAG, string.Format("结束执行订单查询,时间:{0}", DateTime.Now));
                                 #endregion            
                                 break;
                             case Task4TimType.PayTrans2CheckStatus:
                                 #region 检查支付状态
-                                LogUtil.Info(string.Format("开始执行交易查询,时间：{0}", DateTime.Now));
+                                LogUtil.Info(TAG, string.Format("开始执行交易查询,时间：{0}", DateTime.Now));
                                 var payTrans = m.Data.ToJsonObject<PayTrans2CheckStatusModel>();
-                                LogUtil.Info(string.Format("查询交易号：{0}", payTrans.Id));
+                                LogUtil.Info(TAG, string.Format("查询交易号：{0}", payTrans.Id));
                                 //判断支付过期时间
                                 if (m.ExpireTime.AddMinutes(1) >= DateTime.Now)
                                 {
@@ -155,19 +158,19 @@ namespace LocalS.BLL.Task
                                             break;
                                     }
 
-                                    LogUtil.Info(string.Format("交易号：{0},查询支付结果文件:{1}", payTrans.Id, content));
+                                    LogUtil.Info(TAG, string.Format("交易号：{0},查询支付结果文件:{1}", payTrans.Id, content));
                                     MqFactory.Global.PushPayTransResultNotify(IdWorker.Build(IdType.EmptyGuid), payTrans.PayPartner, E_PayTransLogNotifyFrom.Query, content);
                                 }
                                 else
                                 {
-                                    LogUtil.Info(string.Format("交易号：{0},订单支付有效时间过期", payTrans.Id));
+                                    LogUtil.Info(TAG, string.Format("交易号：{0},订单支付有效时间过期", payTrans.Id));
                                     Task4Factory.Tim2Global.Exit(Task4TimType.PayTrans2CheckStatus, payTrans.Id);
                                     foreach (var orderId in payTrans.OrderIds)
                                     {
                                         BizFactory.Order.Cancle(IdWorker.Build(IdType.EmptyGuid), orderId, E_OrderCancleType.PayTimeout, "订单支付有效时间过期");
                                     }
                                 }
-                                LogUtil.Info(string.Format("结束执行交易查询,时间:{0}", DateTime.Now));
+                                LogUtil.Info(TAG, string.Format("结束执行交易查询,时间:{0}", DateTime.Now));
                                 #endregion
                                 break;
                             case Task4TimType.Order2CheckPickupTimeout:
@@ -182,7 +185,7 @@ namespace LocalS.BLL.Task
                             case Task4TimType.PayRefundCheckStatus:
                                 #region 检查订单退款是否成功
                                 var payRefund = m.Data.ToJsonObject<PayRefund2CheckStatusModel>();
-                                LogUtil.Info(string.Format("查询退款单号：{0}", payRefund.Id));
+                                LogUtil.Info(TAG, string.Format("查询退款单号：{0}", payRefund.Id));
                                 //判断支付过期时间
                                 if (m.ExpireTime.AddMinutes(1) >= DateTime.Now)
                                 {
@@ -204,12 +207,12 @@ namespace LocalS.BLL.Task
                                             break;
                                     }
 
-                                    LogUtil.Info(string.Format("退款单号：{0},查询结果文件:{1}", payRefund.Id, content));
+                                    LogUtil.Info(TAG, string.Format("退款单号：{0},查询结果文件:{1}", payRefund.Id, content));
                                     MqFactory.Global.PushPayRefundResultNotify(IdWorker.Build(IdType.EmptyGuid), payRefund.PayPartner, E_PayTransLogNotifyFrom.Query, payRefund.PayTransId, payRefund.Id, content);
                                 }
                                 else
                                 {
-                                    LogUtil.Info(string.Format("退款单号：{0},有效时间过期", payRefund.Id));
+                                    LogUtil.Info(TAG, string.Format("退款单号：{0},有效时间过期", payRefund.Id));
                                     Task4Factory.Tim2Global.Exit(Task4TimType.PayRefundCheckStatus, payRefund.Id);
                                 }
                                 #endregion
@@ -223,7 +226,7 @@ namespace LocalS.BLL.Task
             }
             catch (Exception ex)
             {
-                LogUtil.Error("全局定时任务发生异常", ex);
+                LogUtil.Error(TAG, "全局定时任务发生异常", ex);
             }
             #endregion
         }
