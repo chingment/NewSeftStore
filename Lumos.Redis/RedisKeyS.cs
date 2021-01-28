110e4e8bc69a7bed22e6c7a2e5eec661f2fb2b45
jiecong <jiecong@outlook.com>
2021-01-28 14:01:46 +0800

    .

--- Lumos.Redis/RedisKeyS.cs
+++ Lumos.Redis/RedisKeyS.cs
@@ -8,11 +8,11 @@ namespace Lumos.Redis
 {
     public static class RedisKeyS
     {
-        public static readonly string PRD_SPU_INF = "prd_spu_inf:{0}";//商户所有商品 0：商户ID
-        public static readonly string PRD_SPU_SKEY = "prd_spu_skey:{0}";//商户所有商品ID-条形码映射表 0：商户ID
-        public static readonly string PRD_SKU_INF = "prd_sku_inf:{0}";//商户所有商品 0：商户ID
-        public static readonly string PRD_SKU_SKEY = "prd_sku_skey:{0}";//商户所有商品ID-条形码映射表 0：商户ID
-        public static readonly string PRD_SKU_YKEY = "prd_sku_ykey:{0}";//店铺所有商品ID-条形码映射表 0：店铺ID
+        public static readonly string PRD_SPU_INF = "prd_spu_inf:{0}";//商户所有商品SPU信息表 0：商户ID
+        public static readonly string PRD_SPU_SKEY = "prd_spu_skey:{0}";//商户所有商品SPU-搜索关键字映射表 0：商户ID
+        public static readonly string PRD_SKU_INF = "prd_sku_inf:{0}";//商户所有商品SKU信息表 0：商户ID
+        public static readonly string PRD_SKU_SKEY = "prd_sku_skey:{0}";//商户所有商品SKU-搜索关键字映射表 0：商户ID
+        //public static readonly string PRD_SKU_YKEY = "prd_sku_ykey:{0}";//店铺所有商品ID-条形码映射表 0：店铺ID
         public static readonly string IR_SN = "irsn";//SN序列号增长因子
         public static readonly string IR_MACHINEID = "irmachineid";//SN序列号增长因子
         public static readonly string IR_PICKCODE = "irpc";//取货码增长因子
