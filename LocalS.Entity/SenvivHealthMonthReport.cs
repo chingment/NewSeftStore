c997b46dd846a8898fc256b11bd98d316e2e8508
jiecong <jiecong@outlook.com>
2021-03-26 17:30:40 +0800

    .

---
+++ LocalS.Entity/SenvivHealthMonthReport.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace LocalS.Entity
+{
+
+    public enum E_SenvivHealthMonthStatus
+    {
+        Unknow = 0,
+        WaitBuild = 1,
+        Building = 2,
+        BuildSuccess = 3,
+        BuildFailure = 4,
+        WaitSend = 5,
+        Sending = 6,
+        SendSuccess = 7,
+        SendFailure = 8
+    }
+
+    [Table("SenvivHealthMonth")]
+    public class SenvivHealthMonthReport
+    {
+        public string Id { get; set; }
+        public string SvUserId { get; set; }
+        public string HealthDate { get; set; }
+        public int DayCount { get; set; }
+        public decimal TotalScore { get; set; }
+        public decimal SmSmsc { get; set; }
+        public decimal SmQdsmsc { get; set; }
+        public decimal SmSdsmsc { get; set; }
+        public decimal SmRemsmsc { get; set; }
+        public decimal HrvXzznl { get; set; }
+        public decimal HrvPjhx { get; set; }
+        public decimal HrvPjxl { get; set; }
+        public decimal HrvPjahizs { get; set; }
+        public decimal SmTdcs { get; set; }
+        public bool IsBuild { get; set; }
+        public bool IsSend { get; set; }
+        public bool IsVisit { get; set; }
+        public E_SenvivHealthMonthStatus Status { get; set; }
+        public string Creator { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
