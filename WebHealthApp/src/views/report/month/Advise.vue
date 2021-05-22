6d685f716e8b7ec2207203e9eb39a5fe621f283b
jiecong <jiecong@outlook.com>
2021-05-22 12:38:14 +0800

    .

--- WebHealthApp/src/views/report/month/Advise.vue
+++ WebHealthApp/src/views/report/month/Advise.vue
@@ -1,51 +1,34 @@
 <template>
-  <div class="pg-advise">
-    <div class="lm-card">
-      <div class="lm-card__header">
-        <div class="ct">
-          <img class="icon" src="@/assets/images/icon_advise_yd.png" alt="">
-          <span class="text">运动</span>
-        </div>
-      </div>
-      <div class="lm-card_body">
-        <p>{{ rd.sugByYd }}</p>
-      </div>
-    </div>
+  <div class="pg-energy" style="height:500px">
 
-    <div class="lm-card">
-      <div class="lm-card__header">
-        <div class="ct">
-          <img class="icon" src="@/assets/images/icon_advise_yy.png" alt="">
-          <span class="text">营养</span>
-        </div>
+    <div class="pt1">
+      <img class="pt1_t1" src="@/assets/images/ts/bg_energy_pt1_t1.png" alt="">
+      <img class="pt1_t2" src="@/assets/images/ts/bg_energy_pt1_t2.png" alt="">
+      <img class="pt1_t3" src="@/assets/images/ts/bg_energy_pt1_t2.png" alt="">
+      <div class="at1">
+        <div class="at_a1"><span class="at_title"> 本月综合得分</span><span class="at_score">{{ rd.totalScore }}</span></div>
       </div>
-      <div class="lm-card_body">
-        <p>{{ rd.sugByYy }}</p>
-      </div>
-    </div>
+      <div class="at2">
 
-    <div class="lm-card">
-      <div class="lm-card__header">
-        <div class="ct">
-          <img class="icon" src="@/assets/images/icon_advise_sm.png" alt="">
-          <span class="text">睡眠</span>
+        <div class="explain-card">
+          <div class="explain-card__header">
+            <div class="ct">
+              <div class="title">解释</div>
+              <div class="content">
+                <pre style="white-space: pre-line;">xxxxx</pre>
+              </div>
+            </div>
+          </div>
+          <div class="explain-card_body">
+            <div class="ct">
+              <div class="title">建议：</div>
+              <div class="content"><pre style="white-space: pre-line;">xxx</pre></div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="lm-card_body">
-        <p>{{ rd.sugBySm }}</p>
-      </div>
-    </div>
 
-    <div class="lm-card">
-      <div class="lm-card__header">
-        <div class="ct">
-          <img class="icon" src="@/assets/images/icon_advise_qxyl.png" alt="">
-          <span class="text">情绪压力</span>
-        </div>
       </div>
-      <div class="lm-card_body">
-        <p>{{ rd.sugByQxyl }}</p>
-      </div>
+
     </div>
 
   </div>
@@ -53,34 +36,171 @@
 
 <script>
 
-import { getAdvise } from '@/api/monthreport'
+import { getEnergy } from '@/api/monthreport'
+import DvItem from '@/components/DvItem.vue'
+import echarts from 'echarts'
+
+var chartByTotalScore
+var chartBySm
 
 export default {
-  name: 'Advise',
+  name: 'Energy',
+  components: { DvItem },
   data() {
     return {
       loading: false,
       rd: {
-        sugByYy: '',
-        sugByYd: '',
-        sugBySm: '',
-        sugByQxyl: ''
+        scoreRatio: '',
+        totalScore: '',
+        smSmsc: { color: '', value: '', refRange: '' },
+        smQdsmsc: { color: '', value: '', refRange: '' },
+        smSdsmsc: { color: '', value: '', refRange: '' },
+        smRemsmsc: { color: '', value: '', refRange: '' },
+        hrvXzznl: { color: '', value: '', refRange: '' },
+        hxDcpjhx: { color: '', value: '', refRange: '' },
+        xlDcpjxl: { color: '', value: '', refRange: '' },
+        hxZtcs: { color: '', value: '', refRange: '' },
+        smTdcs: { color: '', value: '', refRange: '' },
+        hxZtahizs: { color: '', value: '', refRange: '' },
+        datePt: [0],
+        totalScorePt: [0],
+        hrvXzznlPt: [0],
+        JbfxXlscfxPt: [0]
       }
     }
   },
+  mounted() {
+    if (!chartByTotalScore) {
+      chartByTotalScore = echarts.init(this.$refs.chart_ByTotalScore, null, { renderer: 'svg' })
+    }
+    if (!chartBySm) {
+      chartBySm = echarts.init(this.$refs.chart_BySm, null, { renderer: 'svg' })
+    }
+    window.addEventListener('beforeunload', this.clearChart)
+  },
   created() {
-    this._getAdvise()
+    this._getEnergy()
+  },
+  beforeDestroy() {
+    if (chartByTotalScore) {
+      chartByTotalScore.clear()
+      chartByTotalScore.dispose()
+      chartByTotalScore = null
+    }
+
+    if (chartBySm) {
+      chartBySm.clear()
+      chartBySm.dispose()
+      chartBySm = null
+    }
+
+    // 清空 resize 事件处理函数
+    window.removeEventListener('resize', this.resize)
+    // 清空 beforeunload 事件处理函数
+    window.removeEventListener('beforeunload', this.clearChart)
   },
   methods: {
-    _getAdvise() {
+    _getEnergy() {
       this.loading = true
-      getAdvise({ rptId: this.$route.query.rptId }).then(res => {
+      getEnergy({ rptId: this.$route.query.rptId }).then(res => {
         if (res.result === 1) {
           var d = res.data
+
           this.rd = d
+
+          this.$nextTick(function() {
+            this.getChartByTotalScore()
+            this.getChartBySm()
+          }, 2000)
         }
         this.loading = false
       })
+    },
+    clearChart() {
+      this.$destroy()
+    },
+    getChartByTotalScore() {
+      var datePt = this.rd.datePt
+      var valuePt = this.rd.totalScorePt
+      var option = {
+        grid: [{
+          x: 45,
+          y: 50,
+          x2: 45,
+          y2: 25
+        }],
+        title: {
+          text: '月度生命趋势图', // 主标题
+          x: 'center',
+          y: 'top'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        xAxis: [{
+          data: datePt,
+          show: true
+        }],
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          type: 'line',
+          name: '分数',
+          showSymbol: false,
+          data: valuePt
+        }
+        ]
+      }
+
+      chartByTotalScore.setOption(option, null)
+    },
+    getChartBySm() {
+      var datePt = this.rd.datePt
+      var valuePt1 = this.rd.hrvXzznlPt
+      var valuePt2 = this.rd.jbfxXlscfxPt
+      var option = {
+        grid: [{
+          x: 45,
+          y: 50,
+          x2: 45,
+          y2: 50
+        }],
+        title: {
+          text: '月度检测趋势图势图',
+          x: 'center',
+          y: 'top'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['心脏总能量', '心率失常风险指数'],
+          // y: 'bottom',
+          top: '90%'
+        },
+        xAxis: [{
+          data: datePt,
+          show: true
+        }],
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          type: 'line',
+          name: '心脏总能量',
+          showSymbol: false,
+          data: valuePt1
+        }, {
+          type: 'line',
+          name: '心率失常风险指数',
+          showSymbol: false,
+          data: valuePt2
+        }
+        ]
+      }
+
+      chartBySm.setOption(option, null)
     }
   }
 }
@@ -88,51 +208,118 @@ export default {
 
 <style lang="scss" scoped>
 
-.pg-advise{
-  padding: 10px;
+.pg-energy{
+  padding: 16px;
+  background: linear-gradient(#ffb24f, #fff)
 }
 
-.lm-card{
-    box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
-    border: 1px solid #ebeef5;
+.pt1{
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
     background-color: #fff;
     color: #303133;
     -webkit-transition: .3s;
     transition: .3s;
-    border-radius: 4px;
+    border-radius: 10px;
     overflow: hidden;
     margin-bottom: 10px;
+    margin-top: 80px;
 }
 
-.lm-card__header{
-    padding: 16px 0px;
-    margin: 0px 16px;
-    border-bottom: 1px solid #ebeef5;
-    -webkit-box-sizing: border-box;
+.pt1_t1{
+width: 100px;
+height: 100px;
+position: absolute;
+right: 100px;
+top: 32px;
+}
+
+.pt1_t2{
+width: 50px;
+    height: 50px;
+    position: absolute;
+    left: 100px;
+    top: 62px;
+}
+
+.pt1_t3{
+    width: 60px;
+    height: 60px;
+    position: absolute;
+    right: 20px;
+    top: 22px;
+}
+
+.at1{
+  height: 150px;
+  background: url('~@/assets/images/ts/bg_energy_pt1_at1.png') no-repeat;
+  background-size: 100% 100%;
+
+  .at_a1{
+        padding-top: 100px;
+    padding-left: 20px;
+    color:#ffbd73;
+
+  .at_title{
+      font-size: 21px;
+    }
+    .at_score{
+      font-size: 32px;
+      margin-left: 10px;
+    }
+  }
+}
+
+.explain-card{
+
+    background-color: #fff;
+    color: #303133;
+    -webkit-transition: .3s;
+    transition: .3s;
+    overflow: hidden;
+    margin-bottom: 10px;
+
+}
+
+.explain-card__header{
+    min-height: 100px;
     box-sizing: border-box;
-
+    background: url('~@/assets/images/ts/bg_explain-card__header.png') no-repeat;;
+    background-size: cover;
 }
 
-.lm-card__header > .ct{
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-
-    .icon{
-      height: 24px;
-      width: 24px;
-    }
-    .text{
-      margin-left:10px;
-    }
-}
-
-.lm-card_body{
+.explain-card__header {
+  .ct{
 padding: 20px;
 
-p{
-  color: #707070;
-  line-height: 24px;
+.title{
+    font-weight: bold;
+    line-height: 32px;
+    font-size: 16px
+}
+
+.content{
+    color: #616161;
+    font-size: 14px;
+    line-height: 21px;
+}
+
+  }
+}
+.explain-card_body{
+
+.ct{
+padding: 20px;
+.title{
+    font-weight: bold;
+    line-height: 32px;
+    font-size: 16px
+}
+
+.content{
+    color: #616161;
+    font-size: 14px;
+    line-height: 21px;
+}
 }
 }
 
