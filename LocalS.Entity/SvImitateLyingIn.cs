0f831c1428484bdcc872a43f1addd68127eb7e39
jiecong <jiecong@outlook.com>
2022-03-11 17:55:36 +0800

    .

---
+++ LocalS.Entity/SvImitateLyingIn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LocalS.Entity
+{
+    [Table("SvImitateLyingIn")]
+    public class SvImitateLyingIn
+    {
+        public string Id { get; set; }
+        public string MerchId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string StartTimeQt { get; set; }
+        public string EndTimeQt { get; set; }
+        public string Creator { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string Mender { get; set; }
+        public DateTime? MendTime { get; set; }
+    }
+}
