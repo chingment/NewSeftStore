4bcaef1d70bb9757ee3314afe69fdd055a6ec4a8
qiuqingwen <chingment@126.com>
2022-05-10 12:56:55 +0800

    .

--- WebHealthApp/src/views/report/day/Indicator.vue
+++ WebHealthApp/src/views/report/day/Indicator.vue
@@ -2,9 +2,9 @@
   <div class="pg-indicator">
   
    <div class="pt1" style="padding:14px">
-    <div ref="chartByMv" style="width: 100%;height: 320px;margin:auto" />
+     <div ref="chartByMv" style="width: 100%;height: 320px;margin:auto" />
 
-
+     <div ref="chartByTd" style="width: 100%;height: 320px;margin:auto" />
 
      <div class="chartByMv-Hxzt"><span class="item">{{rd.hxZtcs.value}}次呼吸暂停</span></div>
 
@@ -72,6 +72,7 @@ import { getIndicator } from '@/api/dayreport'
 import ScoreLevel from '@/components/ScoreLevel.vue'
 import DvCollapse from '@/components/DvCollapse/Index.vue'
 var chartByMv
+var chartByTd
 export default {
   name: '',
   components: {
@@ -108,6 +109,7 @@ export default {
 
         this.$nextTick(function() {
           this.onGetChartByMv()
+          this.onGetChartByTd()
         }, 500)
       }
 
@@ -119,6 +121,12 @@ export default {
       chartByMv.clear()
       chartByMv.dispose()
       chartByMv = null
+    }
+
+    if(chartByTd){
+      chartByTd.clear()
+      chartByTd.dispose()
+      chartByTd = null
     }
   },
   methods: {
@@ -510,6 +518,187 @@ export default {
       }
 
       chartByMv.setOption(option, null)
+    },
+    onGetChartByTd(){
+      if (!chartByTd) {
+        chartByTd = echarts.init(this.$refs.chartByTd, null, { renderer: 'svg' })
+      }
+
+      var sm_point_d = this.rd.smPoint.dataValue
+
+      var sm_point_x_min_t = this.datetimeFormat2(this.rd.smPoint.startTime, -1000 * 60)
+      var sm_point_x_max_t = this.datetimeFormat2(this.rd.smPoint.endTime, +1000 * 60)
+      var sc_time = this.rd.smScsj
+      var rs_time = this.rd.smRssj
+      var qx_time = this.rd.smQxsj
+      var lc_time = this.rd.smLcsj
+
+      var sm_point_xAxis = []
+      var sm_point_xAxis_data = []
+      for (let index = 0; index < sm_point_d.length; index++) {
+        const var1 = sm_point_d[index].startTime
+        const var2 = sm_point_d[index].endTime
+        var type = sm_point_d[index].type
+
+        var var3
+        var color = ''
+
+        if (type === 0) {
+          var3 = 'W'
+          color = this.w_clr
+        } else if (type === 1) {
+          var3 = 'N1'
+          color = this.n1_clr
+        } else if (type === 5) {
+          var3 = 'N3'
+          color = this.n3_clr
+        } else if (type === 4) {
+          var3 = 'R'
+          color = this.r_clr
+        } else if (type === 3) {
+          var3 = 'O'
+          color =this.o_clr
+        } else if (type === 6) {
+          var3 = 'N2'
+          color = this.n2_clr
+        }
+
+        sm_point_xAxis.push(this.datetimeFormat(var1))
+        sm_point_xAxis.push(this.datetimeFormat(var2))
+
+        sm_point_xAxis_data.push({ itemStyle: { normal: { color: color }}, value: [var3, this.datetimeFormat(var1), this.datetimeFormat(var2)] })
+      }
+
+      var option = {
+        grid: [{
+          bottom: '50%',
+          x: 28,
+          y: 50,
+          x2: 28,
+          y2: 50
+        }, {
+          top: '50%',
+          x: 28,
+          y: 50,
+          x2: 28,
+          y2: 50
+
+        }],
+        title: [{
+          left: 'center',
+          text: ''
+        }],
+        xAxis: [{
+          gridIndex: 0,
+          axisTick: {
+            show: false,
+            height: 0
+          },
+          type: 'time',
+          rotate: 0,
+          splitLine: { show: false },
+          interval: 60 * 1000,
+          min: sm_point_x_min_t,
+          max: sm_point_x_max_t,
+          axisLabel: {
+            textStyle: {
+              align: 'center'
+              // verticalAlign: 'bottom'
+            },
+            interval: 0,
+            padding: [-8, 0, 0, 0],
+            formatter: function(value) {
+              function datetimeFormat(longTypeDate) {
+                const date = new Date(longTypeDate)
+                const y = date.getFullYear()
+                let MM = date.getMonth() + 1
+                MM = MM < 10 ? ('0' + MM) : MM
+                let d = date.getDate()
+                d = d < 10 ? ('0' + d) : d
+                let h = date.getHours()
+                h = h < 10 ? ('0' + h) : h
+                let m = date.getMinutes()
+                m = m < 10 ? ('0' + m) : m
+                let s = date.getSeconds()
+                s = s < 10 ? ('0' + s) : s
+                return y + '/' + MM + '/' + d + ' ' + h + ':' + m
+              }
+
+              function getMin(longTypeDate) {
+                const date = new Date(longTypeDate)
+                const y = date.getFullYear()
+                let MM = date.getMonth() + 1
+                MM = MM < 10 ? ('0' + MM) : MM
+                let d = date.getDate()
+                d = d < 10 ? ('0' + d) : d
+                let h = date.getHours()
+                h = h < 10 ? ('0' + h) : h
+                let m = date.getMinutes()
+                m = m < 10 ? ('0' + m) : m
+                let s = date.getSeconds()
+                s = s < 10 ? ('0' + s) : s
+                return h + ':' + m
+              }
+
+              if (datetimeFormat(value) === sc_time) {
+                // console.log('上床')
+                return '|\n|\n|\n上床 ' + getMin(value)
+              } else if (datetimeFormat(value) === rs_time) {
+                // console.log('入睡')
+                return '|\n入睡 ' + getMin(value)
+              } else if (datetimeFormat(value) === qx_time) {
+                // console.log('清醒')
+                return '|\n清醒 ' + getMin(value)
+              } else if (datetimeFormat(value) === lc_time) {
+                // console.log('离床')
+                return '|\n|\n|\n离床 ' + getMin(value)
+              }
+              return ''
+            }
+          }
+        }],
+        yAxis: [{
+          splitLine: { show: false },
+          gridIndex: 0,
+          data: ['N3', 'N2', 'N1', 'R', 'W', 'O']
+        }],
+        series: [
+          {
+            xAxisIndex: 0,
+            yAxisIndex: 0,
+            type: 'custom',
+            renderItem: function(params, api) {
+              var categoryIndex = api.value(0)
+              var start = api.coord([api.value(1), categoryIndex])
+              var end = api.coord([api.value(2), categoryIndex])
+              var height = 12
+              return {
+                type: 'rect',
+                shape: echarts.graphic.clipRectByRect({
+                  x: start[0],
+                  y: start[1] - height / 2,
+                  width: end[0] - start[0],
+                  height: height
+                }, {
+                  x: params.coordSys.x,
+                  y: params.coordSys.y,
+                  width: params.coordSys.width,
+                  height: params.coordSys.height
+                }),
+                style: api.style()
+              }
+            },
+            encode: {
+              x: [1, 2],
+              y: 0
+            },
+            data: sm_point_xAxis_data
+          }]
+      }
+
+      chartByTd.setOption(option, null)
+
+
     },
     datetimeFormat(longTypeDate) {
       const date = new Date(longTypeDate * 1000)
